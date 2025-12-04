971685d47b3041b793cb200d258c748446077f5a
pscruggs3 <pscruggs3@gatech.edu>
2025-12-04 15:05:13 -0600

    chore: add copyright notice and license information to multiple files

--- doc.go
+++ doc.go
@@ -1,3 +1,17 @@
+// Copyright 2025 Patrick J. Scruggs
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 // Package slogcpadapter wires [github.com/pjscruggs/slogcp] into
 // [github.com/grpc-ecosystem/go-grpc-middleware/v2] logging interceptors.
 // It implements logging.Logger by forwarding records to a slog.Logger backed by a
