63580bfee2fabee06b10d64d03e33c87867f2cbf
pjscruggs <PatrickJScruggs@gmail.com>
2026-09-19 16:36:20 -0500

    fix(release): publish the core dependency repair as a patch

--- version.go
+++ version.go
@@ -16,4 +16,4 @@ package slogcpadapter
 
 // Version is the current version of the slogcp gRPC adapter.
 // It follows semantic versioning (https://semver.org/).
-const Version = "v2.0.0"
+const Version = "v2.0.1"
